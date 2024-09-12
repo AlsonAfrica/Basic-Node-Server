ffad7c2c4eae4892ba7d9783b16349f5e3ec9444
Nhlakanipho <nhalakaniphoradebe337@gmail.com>
2024-09-12 13:43:47 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors()); 
+app.use(express.json()); 
+
+// A simple API route
+app.get('/api/message', (req, res) => {
+    res.json({ message: "Hello from the backend!" });
+});
+
+// Start the server
+const PORT = 5002;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+
+
