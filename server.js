6b353854cfb446d4a761313d740f3f9fc9fe4795
Nhlakanipho <nhalakaniphoradebe337@gmail.com>
2024-09-16 13:56:05 +0200

    Update: the servers uses basic http module

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
-const express = require('express');
-const cors = require('cors');
+// const express = require('express');
+// const cors = require('cors');
 
-const app = express();
-app.use(cors()); 
-app.use(express.json()); 
+// const app = express();
+// app.use(cors()); 
+// app.use(express.json()); 
 
-// A simple API route
-app.get('/api/message', (req, res) => {
-    res.json({ message: "Hello from the backend!" });
-});
+// // A simple API route
+// app.get('/api/message', (req, res) => {
+//     res.json({ message: "Hello from the backend!" });
+// });
 
-// Start the server
+// // Start the server
+// const PORT = 5002;
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
+
+// imports node http module from node 
+const http = require ('http');  
 const PORT = 5002;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
 
+const server = http.createServer((req, res)=>{res.end("Hello!");});
 
+server.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
 
